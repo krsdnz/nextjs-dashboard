88c72e705b44d7d5e55a39a339dbe359a39d8763
Krsdnz <krsdkrdnz@gmail.com>
2024-08-21 12:44:32 +0300

    Fetching Data, chapter 7

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,6 +8,15 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+// Fetch the last 5 invoices, sorted by date
+const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
+const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
+const data = await sql<LatestInvoiceRaw>`
+  SELECT invoices.amount, customers.name, customers.image_url, customers.email
+  FROM invoices
+  JOIN customers ON invoices.customer_id = customers.id
+  ORDER BY invoices.date DESC
+  LIMIT 5`;
 
 export async function fetchRevenue() {
   try {
